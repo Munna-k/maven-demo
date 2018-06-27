a4e32f65a7a79f929dfe41294ba8e5302d4299f2
Munna-k <40426953+Munna-k@users.noreply.github.com>
2018-06-27 12:42:39 +0530

    Update demomaven.java

--- src/main/java/demomaven.java
+++ src/main/java/demomaven.java
@@ -3,6 +3,16 @@ public class demomaven {
 
 	public static void main(String[] args) {
 		
+//		int n=10;
+//		for(int i=0;i<=n;i++)
+//		{
+//			if(i%2==0)
+//			{
+//			System.out.println("even="+i);
+//		}
+//			else
+//				System.out.println("odd="+i);
+//		}
 		System.out.println("Hello world");
 	}
 
